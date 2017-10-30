9da0044f9d73a3d472ec37df2ad34e00ef864943
Nada El Hakim <elhakim.nada88@gmail.com>
2017-10-30 07:54:02 +0200

    ignore app.js

--- spotify-api/app.js
+++ spotify-api/app.js
@@ -3,8 +3,8 @@ var app = express();
 var request = require('request');
 var bodyParser = require('body-parser')
 
-var client_id = '1141cb8f9e66467d8cf514b799a3773e'; // Your client id
-var client_secret = '6468c9dc1d48472a915d3552d0c27a36'; // Your secret
+var client_id = ''; // Your client id
+var client_secret = ''; // Your secret
 var redirect_uri = 'http://localhost/callback'; // Your redirect uri
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
